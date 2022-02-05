00608b82b80fd000aa97a3633b295a36ba3f6793
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-06 01:55:13 +0200

    added styles to task-2

--- src/componenets/Statistics/Statistics.jsx
+++ src/componenets/Statistics/Statistics.jsx
@@ -3,20 +3,30 @@ import propTypes from 'prop-types';
 
 const Statistics = ({ title, stats }) => {
     return (
-        <section className={s.statistics}>
-            {title && <h2 className={s.title}>{title}</h2>}
-
-            <ul className={s.statList}>
-                {stats.map(prop => (
-                    <li className={s.item} key={prop.id}>
-                        <span className={s.label}>{prop.label}</span>
-                        <span className={s.percentage}>{prop.percentage}</span>
-                    </li>
-                )
-                )}
-            </ul>
-        </section>
+        <>
+            <h2>TASK 2</h2>
+            <section className={s.statistics}>
+                <div className={s.statisticsCard}>
+                    {title && <h2 className={s.title}>{title}</h2>}
+                    <ul className={s.statList}>
+                        {stats.map(prop => (
+                            <li className={s.item} key={prop.id} style={{ backgroundColor: getRandomHexColor() }}>
+                                <span className={s.label}>{prop.label}</span> <br />
+                                <span className={s.percentage}>{prop.percentage}%</span>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                </div>
+            </section>
+        </>
     )
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`
 }
 
 Statistics.propTypes = {
