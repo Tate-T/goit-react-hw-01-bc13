f246ed944a20ee5c1d811db93e5a5bde92af6089
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-06 02:45:16 +0200

    added stules to task-4

--- src/componenets/TransactionHistory/TransactionHistory.jsx
+++ src/componenets/TransactionHistory/TransactionHistory.jsx
@@ -3,27 +3,30 @@ import propTypes from 'prop-types';
 
 const TransactionHistory = ({ items }) => {
     return (
-        <table className={s.transactionHistory}>
-            <thead>
-                <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
-                </tr>
-            </thead>
+        <>
+            <h2>TASK 4</h2>
+            <table className={s.transactionHistory}>
+                <thead className={s.transactionHead}>
+                    <tr>
+                        <th className={s.item}>Type</th>
+                        <th className={s.item}>Amount</th>
+                        <th className={s.item}>Currency</th>
+                    </tr>
+                </thead>
 
-            <tbody>
-                {items.map(
-                    ({ id, type, amount, currency }) => (
-                        <tr key={id}>
-                            <td>{type}</td>
-                            <td>{amount}</td>
-                            <td>{currency}</td>
-                        </tr>
-                    )
-                )}
-            </tbody>
-        </table>
+                <tbody >
+                    {items.map(
+                        ({ id, type, amount, currency }) => (
+                            <tr key={id} className={s.transactionTable}>
+                                <td className={s.item}>{type}</td>
+                                <td className={s.item}>{amount}</td>
+                                <td className={s.item}>{currency}</td>
+                            </tr>
+                        )
+                    )}
+                </tbody>
+            </table>
+        </>
     )
 }
 
